59c774dbf4c57cc3ccc92d8fb9c8d8a0ffdbeaec
Kristian Helgesen <kristian@ardoq.com>
2015-08-20 14:32:07 +0200

    server running

---
+++ src/main/java/com/ardoq/addon/maven/Main.java
@@ -0,0 +1,10 @@
+package com.ardoq.addon.maven;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception{
+		MavenAddonApplication app = new MavenAddonApplication();
+		app.run(args);
+	}
+	
+}
--- src/main/java/com/ardoq/addon/maven/MavenAddonConfiguration.java
+++ src/main/java/com/ardoq/addon/maven/MavenAddonConfiguration.java
@@ -1,17 +1,14 @@
 package com.ardoq.addon.maven;
 
-import io.dropwizard.Configuration;
-import io.dropwizard.client.HttpClientConfiguration;
-import io.dropwizard.client.JerseyClientConfiguration;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-import com.bazaarvoice.dropwizard.assets.AssetsBundleConfiguration;
-import com.bazaarvoice.dropwizard.assets.AssetsConfiguration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class MavenAddonConfiguration extends Configuration implements AssetsBundleConfiguration{
+import io.dropwizard.Configuration;
+import io.dropwizard.client.HttpClientConfiguration;
+
+public class MavenAddonConfiguration extends Configuration {
 	
 	
 	@Valid
@@ -19,28 +16,9 @@ public class MavenAddonConfiguration extends Configuration implements AssetsBund
     @JsonProperty
     private HttpClientConfiguration httpClient = new HttpClientConfiguration();
 	
-    @Valid
-    @NotNull
-    @JsonProperty
-    private JerseyClientConfiguration jerseyClient = new JerseyClientConfiguration();
-
-
-    @Valid
-    @NotNull
-    @JsonProperty
-    private final AssetsConfiguration assets = new AssetsConfiguration();
-
-    
-    public AssetsConfiguration getAssetsConfiguration() {
-        return assets;
-    }
 	
     public HttpClientConfiguration getHttpClientConfiguration() {
         return httpClient;
-    }
-    
-    public JerseyClientConfiguration getJerseyClientConfiguration() {
-        return jerseyClient;
     }
 
 
